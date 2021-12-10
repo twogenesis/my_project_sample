1072134928a6f46a36f1f9b7434ad34d5f8d998b
twogenesis <twogenesis@naver.com>
2021-12-10 10:13:23 +0900

    포트 변경 및 메인 컨트롤러 추가

---
+++ src/main/java/com/twogenesis/sample_project/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.twogenesis.sample_project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String getMain() {
+        return "/index";
+    }
+}
